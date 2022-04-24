3df8d40b7fcf87fcb3d34516e7dd5096200a8f67
张荣平 <1473166181@qq.com>
2022-04-24 09:15:24 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { gsap } from 'gsap'
 import './utils/SplitText'
+import './utils/MorphSVGPlugin.min.js'
+
 import './assets/theme/scss/index.scss'
+gsap.registerPlugin(MorphSVGPlugin)
 window.gsap = gsap
 
 createApp(App).mount('#app')
